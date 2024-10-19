2c2b38807d930cfe3d03624fdb22eda7f31408f1
Tim Bramming <90619736+TimmyTheAverage@users.noreply.github.com>
2024-10-19 12:53:45 +0200

    Fuck this

---
+++ Exercises/Exercise4/ExampleGame/main.cpp
@@ -0,0 +1,53 @@
+#include "sre/SDLRenderer.hpp"
+#include "sre/SpriteAtlas.hpp"
+
+#include "Engine/MyEngine.h"
+
+#include "Game/ComponentController.h"
+#include "Game/ComponentRendererSprite.h"
+
+void InitGame();
+void ProcessEvents(SDL_Event& event);
+void Update(float deltaTime);
+void Render();
+
+MyEngine::Engine engine;
+
+glm::vec2 window_size = glm::vec2(1280, 720);
+sre::SDLRenderer renderer;
+sre::Camera camera;
+std::shared_ptr<sre::SpriteAtlas> atlas;
+
+int main() {
+	renderer.frameRender = Render;
+	renderer.frameUpdate = Update;
+	renderer.keyEvent = ProcessEvents;
+
+	renderer.setWindowSize(window_size);
+	renderer.init();
+	camera.setWindowCoordinates();
+
+	atlas = sre::SpriteAtlas::create("data/astroids.json", "data/astroids.png");
+
+	auto gameObject = engine.CreateGameObject("Player");
+	auto componentController = std::shared_ptr<ExampleGame::ComponentController>(new ExampleGame::ComponentController());
+	auto componentRenderer = std::make_shared<ExampleGame::ComponentRendererSprite>();
+	gameObject->AddComponent(componentController);
+	gameObject->AddComponent(componentRenderer);
+
+	componentRenderer->sprite = atlas->get("enemyBlue3.png");
+
+	engine.Init();
+
+	renderer.startEventLoop();
+}
+
+void ProcessEvents(SDL_Event& event) { }
+
+void Update(float deltaTime) {
+	engine.Update(deltaTime);
+}
+
+void Render() {
+	engine.Render();
+}
